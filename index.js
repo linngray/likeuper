ac101365b88649ac590c94ca0f796a3b41e2a04c
Linn <132255897+linngray@users.noreply.github.com>
2024-07-04 14:38:16 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+require('dotenv').config()
+const { Client } = require('discord.js-selfbot-v13')
+const client = new Client()
+const { keep_alive } = require("./keep_alive");
+
+client.on('ready', async () => {
+    console.log(`Logged in as ${client.user.tag}!`)
+
+    const channel = await client.channels.fetch(process.env.BUMP_CHANNEL)
+    
+    async function bump() {
+        await channel.sendSlash('302050872383242240', 'bump')
+        console.count('Bumped!')
+    }
+
+    function loop() {
+        // Prevent Detection
+        var randomNum = Math.round(Math.random() * (9000000 - 7200000 + 1)) + 7200000
+        setTimeout(function () {
+            bump()
+            loop()
+        }, randomNum)
+    }
+    
+    bump()
+    loop()
+})
+
+client.login(process.env.TOKEN)
