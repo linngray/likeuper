a8e5bb902a5906e27c612960367e906bb5335092
Linn <132255897+linngray@users.noreply.github.com>
2024-08-25 13:06:53 +0300

    Deleted Skipping

--- index.js
+++ index.js
@@ -1,57 +1,45 @@
-require('dotenv').config();
-const { Client } = require('discord.js-selfbot-v13');
-const client = new Client();
+require('dotenv').config()
+const { Client } = require('discord.js-selfbot-v13')
+const client = new Client()
 const { keep_alive } = require("./keep_alive");
 
 client.on('ready', async () => {
-    console.log(`Logged in as ${client.user.tag}!`);
+    console.log(`Logged in as ${client.user.tag}!`)
 
-    const channel = await client.channels.fetch(process.env.BUMP_CHANNEL);
-
-    function isRestrictedTime() {
-        const now = new Date();
-        const hours = now.getUTCHours() + 3; // Добавляем 3 часа для перевода в московское время
-        return (hours >= 1 && hours < 6);
-    }
+    const channel = await client.channels.fetch(process.env.BUMP_CHANNEL)
 
     async function bump() {
-        if (isRestrictedTime()) {
-            console.log('Skipping bump due to restricted time interval (Moscow time)');
-            return;
-        }
-        await channel.sendSlash('575776004233232386', 'like');
-        console.count('Liked!');
+        await channel.sendSlash('575776004233232386', 'like')
+        console.count('Liked!')
     }
 
     async function otherBump() {
-        if (isRestrictedTime()) {
-            console.log('Skipping otherBump due to restricted time interval (Moscow time)');
-            return;
-        }
-        await channel.sendSlash('302050872383242240', 'bump');
-        console.count('Bumped!');
+        await channel.sendSlash('302050872383242240', 'bump')
+        console.count('Bumped!')
     }
 
     function loop() {
-        var randomNum = Math.round(Math.random() * (19800000 - 14400000 + 1)) + 14400000;
+        // Prevent Detection
+        var randomNum = Math.round(Math.random() * (19800000 - 14400000 + 1)) + 14400000
         setTimeout(function () {
-            bump();
-            loop();
-        }, randomNum);
+            bump()
+            loop()
+        }, randomNum)
     }
 
     function otherLoop() {
-        var randomNum = Math.round(Math.random() * (10800000 - 7200000 + 1)) + 7200000;
+        // Prevent Detection
+        var randomNum = Math.round(Math.random() * (10800000 - 7200000 + 1)) + 7200000
         setTimeout(function () {
-            otherBump();
-            otherLoop();
-        }, randomNum);
+            otherBump()
+            otherLoop()
+        }, randomNum)
     }
 
-    bump();
-    loop();
-    otherBump();
-    otherLoop();
-});
+    bump()
+    loop()
+    otherBump()
+    otherLoop()
+})
 
-client.login(process.env.TOKEN);
+client.login(process.env.TOKEN)
